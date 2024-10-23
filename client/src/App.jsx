46a096153a0a3337ed28fd41cc41af0d1b7c4627
Jonathan Marshall <jwmarsh16@gmail.com>
2024-10-23 15:00:25 -0500

    added Home and NavBar to components and Routes to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './NavBar';
+import PlayerList from './PlayerList';
+import PlayerDetail from './PlayerDetail';
+import AddReview from './AddReview';
+import UserProfile from './UserProfile';
+import UserList from './UserList'; 
+import Register from './Register';
+import Login from './Login';
+import Home from './Home';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path="/players/:id" component={PlayerDetail} />
+        <Route path="/players" component={PlayerList} />
+        <Route path="/add-review/:playerId" component={AddReview} />
+        <Route path="/profile/:id" component={UserProfile} />
+        <Route path="/users" component={UserList} /> 
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/" component={Home} exact />
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/players">Players</Link></li>
+        <li><Link to="/users">Users</Link></li> 
+        <li><Link to="/register">Register</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
