0934685889e422513b646bd5ceef1172d8e2174a
Jonathan Marshall <jwmarsh16@gmail.com>
2024-11-01 15:05:56 -0500

    updated for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-   proxy: {
-    '/api': {
-      target: 'http://localhost:5555',
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, ''),
-    },
-   },
-  }
+  build: {
+    outDir: 'dist',  // Keeps the output in client/dist
+  },
 })
+
+
+
+// https://vitejs.dev/config/
+//export default defineConfig({
+  //plugins: [react()],
+  //server: {
+   //proxy: {
+    //'/api': {
+      //target: 'http://localhost:5555',
+      //changeOrigin: true,
+      //rewrite: (path) => path.replace(/^\/api/, ''),
+    //},
+   //},
+  //}
+//})
